68c73647e144d438b746f1eba4cfd361a1de0ce2
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2025-09-25 03:38:48 +0530

    sss

--- envloader.sh
+++ envloader.sh
@@ -2,7 +2,7 @@
 
 if [[ -n "$CONFIG_ENV_URL" ]]; then
     echo "Downloading config.env from $CONFIG_ENV_URL..."
-    wget -q "$CONFIG_ENV_URL" -O /root/LyFE/config.env
+    curl -sSL "$CONFIG_ENV_URL" -o /root/LyFE/config.env
 else
     echo "CONFIG_ENV_URL is not set, skipping download."
 fi
